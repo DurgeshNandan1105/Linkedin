73bb6a6314904d592ce5d4d1e3a324af06458a10
Durgesh Nandan <durgeshnandan748@gmail.com>
2026-09-03 09:47:45 +0530

    setting up routes

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import Profile from "../models/Profile.model.js";
 import bcrypt from 'bcrypt';
+import crypto from 'crypto';
 
 export const register = async (req,res) => {
     try{
@@ -26,3 +27,51 @@ export const register = async (req,res) => {
         return res.status(500).json({message: error.message})
     }
 }
+
+export const login = async (req,res) => {
+    try {
+        const { email, password} = req.body;
+        if(!email || !password) return res.status(400).json({message: "All fields are required"})
+
+            const user = await User.findOne({
+                email
+            });
+
+            if (!user) return res.status(404).json({message:"User does not exist"})
+                const isMatch = await bcrypt.compare(password, user.password);
+                if (!isMatch) return res.status(400).json({message:"Invalid Credentials"});
+
+                const token = crypto.randomBytes(32).toString("hex");
+
+                await User.updateOne({_id: user._id}, {token});
+
+                return res.json({token})
+
+        }catch(error){
+return res.status(500).json({
+        message: error.message
+    });
+
+        }
+    }
+
+
+    export const uploadProfilePicture = async(req,res)=> {
+        const { token } = req.body;
+
+        try {
+
+            const user = await User.findOne({ token: token});
+
+            if(!user) {
+                return res.status(404).json({message: "user not found"})
+            }
+            user.profilePicture = req.file.filename;
+
+            await user.save();
+            return res.json({message: "Profile Picture Updated"});
+
+        } catch (error) {
+            return res.status(500).json({message: error.message})
+        }
+    }
